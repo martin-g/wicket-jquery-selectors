ac61f565e8fd87555d021f7a80fe7a69eb296d93
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-05-10 23:00:25 +0300

    Fixes #11 - Use Jackson's com.fasterxml.jackson.databind.util.RawValue when needing to serialize as raw
    X-Refs:
    - https://github.com/FasterXML/jackson-databind/issues/737
    - https://github.com/FasterXML/jackson-databind/issues/743

--- src/main/java/de/agilecoders/wicket/jquery/util/serializer/RawSerializer.java
+++ src/main/java/de/agilecoders/wicket/jquery/util/serializer/RawSerializer.java
@@ -3,6 +3,7 @@ package de.agilecoders.wicket.jquery.util.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.util.RawValue;
 import de.agilecoders.wicket.jquery.util.Json;
 
 import java.io.IOException;
@@ -15,6 +16,22 @@ import java.io.IOException;
 public class RawSerializer extends JsonSerializer<Json.RawValue> {
     @Override
     public void serialize(Json.RawValue value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
-        jsonGenerator.writeObject(value.value());
+        jsonGenerator.writeObject(new RV(value.value()));
+    }
+
+    /**
+     * An extension of com.fasterxml.jackson.databind.util.RawValue that delegates
+     * toString() to the rawValue
+     */
+    private static class RV extends RawValue {
+
+        public RV(String v) {
+            super(v);
+        }
+
+        @Override
+        public String toString() {
+            return rawValue().toString();
+        }
     }
 }
