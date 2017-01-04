c5c5b42a04013eed865715fbf373466244868a9e
Ronny Pscheidl <Pscheidl@koemac022.koeln.ads.risnet.de>
2017-01-04 10:15:05 +0100

    Revert "http://alexandregiannini.blogspot.de/2011/05/escaping-strings-for-jquery-selectors.html - "If you wish to use any of the meta-characters ( such as !"#$%&'()*+,./:;<=>?@[\]^`{|}~ ) as a literal part of a name, you must escape the character with two backslashes: \\.""
    This reverts commit 9bec454738aa4a544a1e8163c01534c214ff12cb.

--- src/main/java/de/agilecoders/wicket/jquery/util/Strings2.java
+++ src/main/java/de/agilecoders/wicket/jquery/util/Strings2.java
@@ -1,10 +1,10 @@
 package de.agilecoders.wicket.jquery.util;
 
-import java.text.CharacterIterator;
-import java.text.StringCharacterIterator;
-
 import org.apache.wicket.Component;
 import org.apache.wicket.util.lang.Args;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 
 /**
  * helper class to handle string interaction
@@ -12,73 +12,73 @@ import org.apache.wicket.util.lang.Args;
  * @author miha
  */
 public final class Strings2 {
-	private static char[] ESCAPE_CHARS = new char[] { '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[',
-			']', '\\', '^', '`', '{', '}', '|', '~', '\'' };
+    private static char[] ESCAPE_CHARS = new char[] {
+            '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.',
+            '/', ':', ';', '<', '>', '=', '?', '@', '[', ']', '\\', '^', '`',
+            '{', '}', '|', '~', '\''
+    };
 
-	/**
-	 * ensures a non null value.
-	 *
-	 * @param value
-	 *            string value to transform to an empty string if it's null
-	 * @return non null value
-	 */
-	public static String nullToEmpty(final String value) {
-		return value != null ? value : "";
-	}
+    /**
+     * ensures a non null value.
+     *
+     * @param value string value to transform to an empty string if it's null
+     * @return non null value
+     */
+    public static String nullToEmpty(final String value) {
+        return value != null ? value : "";
+    }
 
-	/**
-	 * Returns a markup id that is JQuery-safe and could be used as a selector.
-	 *
-	 * @param component
-	 *            the component which markup id should be return
-	 * @return the component's markup id that is escaped so that it could be used as JQuery selector
-	 */
-	public static CharSequence getMarkupId(final Component component) {
-		Args.notNull(component, "component");
-		String markupId = component.getMarkupId(true);
-		return escapeMarkupId(markupId);
-	}
+    /**
+     * Returns a markup id that is JQuery-safe and could be used as a selector.
+     *
+     * @param component the component which markup id should be return
+     * @return the component's markup id that is escaped so that it could be used as JQuery selector
+     */
+    public static CharSequence getMarkupId(final Component component) {
+        Args.notNull(component, "component");
+        String markupId = component.getMarkupId(true);
+        return escapeMarkupId(markupId);
+    }
 
-	/**
-	 * Returns a markup id that is JQuery-safe and could be used as a selector.
-	 *
-	 * @param markupId
-	 *            the markup id to escape
-	 * @return the component's markup id that is escaped so that it could be used as JQuery selector
-	 */
-	public static CharSequence escapeMarkupId(final CharSequence markupId) {
-		Args.notNull(markupId, "markupId");
+    /**
+     * Returns a markup id that is JQuery-safe and could be used as a selector.
+     *
+     * @param markupId the markup id to escape
+     * @return the component's markup id that is escaped so that it could be used as JQuery selector
+     */
+    public static CharSequence escapeMarkupId(final CharSequence markupId) {
+        Args.notNull(markupId, "markupId");
 
-		// create pattern for: !"#$%&'()*+,./:;<=>?@[\]^`{|}~
-		final StringCharacterIterator iterator = new StringCharacterIterator(markupId.toString());
-		final StringBuilder result = new StringBuilder((int) (markupId.length() * 1.5));
-		final String escape = "\\\\";
+        // create pattern for: !"#$%&'()*+,./:;<=>?@[\]^`{|}~
+        final StringCharacterIterator iterator = new StringCharacterIterator(markupId.toString());
+        final StringBuilder result = new StringBuilder((int) (markupId.length() * 1.5));
+        final String escape = "\\";
 
-		char c = iterator.current();
-		while (c != CharacterIterator.DONE) {
-			boolean escaped = false;
-			for (char x : ESCAPE_CHARS) {
-				if (x == c) {
-					result.append(escape).append(c);
-					escaped = true;
-					break;
-				}
-			}
+        char c = iterator.current();
+        while (c != CharacterIterator.DONE) {
+            boolean escaped = false;
+            for (char x : ESCAPE_CHARS) {
+                if (x == c) {
+                    result.append(escape).append(c);
+                    escaped = true;
+                    break;
+                }
+            }
 
-			if (!escaped) {
-				result.append(c);
-			}
+            if (!escaped) {
+                result.append(c);
+            }
 
-			c = iterator.next();
-		}
+            c = iterator.next();
+        }
 
-		return result.toString();
-	}
+        return result.toString();
+    }
 
-	/**
-	 * private constructor to prevent instantiation of util class.
-	 */
-	private Strings2() {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * private constructor to prevent instantiation of util class.
+     */
+    private Strings2() {
+        throw new UnsupportedOperationException();
+    }
 }
