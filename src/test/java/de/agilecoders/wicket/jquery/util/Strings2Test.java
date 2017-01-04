0a4cd3f2e5dc6d78b105980927b5dcef3b7f1ab1
Ronny Pscheidl <Pscheidl@koemac022.koeln.ads.risnet.de>
2017-01-04 10:14:14 +0100

    Revert "http://alexandregiannini.blogspot.de/2011/05/escaping-strings-for-jquery-selectors.html - "If you wish to use any of the meta-characters ( such as !"#$%&'()*+,./:;<=>?@[\]^`{|}~ ) as a literal part of a name, you must escape the character with two backslashes: \\.""
    This reverts commit 34adacc7875d0978f6f66bfbb504336a21af0981.

--- src/test/java/de/agilecoders/wicket/jquery/util/Strings2Test.java
+++ src/test/java/de/agilecoders/wicket/jquery/util/Strings2Test.java
@@ -1,9 +1,9 @@
 package de.agilecoders.wicket.jquery.util;
 
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
-import org.junit.Test;
 
 /**
  * tests the {@link de.agilecoders.wicket.jquery.util.Strings2} class
@@ -12,13 +12,13 @@ import org.junit.Test;
  */
 public class Strings2Test {
 
-	@Test
-	public void escapeCharacters() {
-		assertThat(
-				Strings2.escapeMarkupId("!\"#$%&'()*+,./:;<=>?@[\\]^`{|}~").toString(),
-				is("\\\\!\\\\\"\\\\#\\\\$\\\\%\\\\&\\\\'\\\\(\\\\)\\\\*\\\\+\\\\,\\\\.\\\\/\\\\:\\\\;\\\\<\\\\=\\\\>\\\\?\\\\@\\\\[\\\\\\\\\\]\\\\^\\\\`\\\\{\\\\|\\\\}\\\\~"));
+    @Test
+    public void escapeCharacters() {
+        assertThat(Strings2.escapeMarkupId("!\"#$%&'()*+,./:;<=>?@[\\]^`{|}~").toString(),
+                   is("\\!\\\"\\#\\$\\%\\&\\'\\(\\)\\*\\+\\,\\.\\/\\:\\;\\<\\=\\>\\?\\@\\[\\\\\\]\\^\\`\\{\\|\\}\\~"));
 
-		assertThat(Strings2.escapeMarkupId("my.custom.markup-id").toString(), is("my\\\\.custom\\\\.markup-id"));
-	}
+        assertThat(Strings2.escapeMarkupId("my.custom.markup-id").toString(),
+                   is("my\\.custom\\.markup-id"));
+    }
 
 }
